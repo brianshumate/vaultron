43ed83754aa764c8f8a0b87cff901cd56edc0b07
siddarth <siddarth@hashicorp.com>
2022-04-28 18:23:36 +0200

    modified versions.tf

--- black_lion/versions.tf
+++ black_lion/versions.tf
@@ -2,10 +2,12 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 2.11.0"
+      version = "~> 2.16.0"
     }
     template = {
       source = "hashicorp/template"
+      version = "~> 2.2.0"
+
     }
   }
   required_version = ">= 0.13"
--- red_lion/versions.tf
+++ red_lion/versions.tf
@@ -2,13 +2,15 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 2.11.0"
+      version = "~> 2.16.0"
     }
     random = {
       source = "hashicorp/random"
+      version = "~> 3.1.1"  
     }
     template = {
       source = "hashicorp/template"
+      version = "~> 2.2.0"
     }
   }
   required_version = ">= 0.13"
